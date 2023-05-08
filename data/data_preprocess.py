e43095f9103b2191d3684508ce2fe32087ec919a
Bálint Turi <72875354+tubali12345@users.noreply.github.com>
2023-05-08 13:42:43 +0200

    added resizing images to 720p

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from pathlib import Path
+from PIL import Image
+from tqdm import tqdm
 
 
 def data_preprocess(data_path: str) -> None:
@@ -16,7 +19,18 @@ def data_preprocess(data_path: str) -> None:
         if i == len(df) - 1:
             new_df = new_df.append({'id': prev_id, 'predicted': predictions}, ignore_index=True)
     new_df.to_csv('train_solutions_preprocessed.csv', index=False, header=True)
+  
+
+def resize_images(image_path: str):      
+    image = Image.open(file_path)
+      
+    # resized = image.resize((1920, 1080))
+    resized = image.resize((1280, 720))
+    esized.save(file_path.replace('/images/', '/images_compressed/'))
+    return
 
 
 if __name__ == '__main__':
     data_preprocess('train_solutions.csv')
+    for file in tqdm(Path('/home/turib/multi_label_image_classification/data/images').rglob(f'*_orig.jpg')):
+    resize_images(str(file))
