3168289833c5ae96677fc0bf2fa10bc07e5fb145
Bálint Turi <72875354+tubali12345@users.noreply.github.com>
2023-05-08 14:00:32 +0200

    added hamming loss for printing

--- train/train_loop.py
+++ train/train_loop.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import torch
 from tqdm import tqdm
+from sklearn.metrics import hamming_loss
 
 from val_loop import validation
 
@@ -26,20 +27,22 @@ def train_loop(model,
     mini_batch = 500
     for epoch in range(load_epoch + 1, num_epochs + 1):
         model.train()
-        correct = 0
-        total = 0
         for i, batch in enumerate(tqdm(train_ds, desc=f'Training... Epoch {epoch}')):
             x, y = batch
             out = model(x.to(device))
 
-            total += y.size(0)
-
             loss = loss_fn(out, y.to(device))
+            
+            out_numpy = out.cpu().detach().numpy()
+            out_numpy[out_numpy < 0.5] = 0
+            out_numpy[out_numpy>= 0.5] = 1
+            hamm_loss = hamming_loss(y.cpu().numpy(), out_numpy)
+            
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             lr_sched.step()
             if i % mini_batch == 0:
-                print(f'Current loss: {round(loss.item(), 4)}, Current acc: {round(correct/total, 4)}, current LR: {round(lr_sched.get_last_lr()[0], 6)}')
+                print(f'Current loss: {round(loss.item(), 4)}, Current hamming loss: {round(hamm_loss, 4)}, current LR: {round(lr_sched.get_last_lr()[0], 6)}')
         # validation(model, valid_ds, loss_fn, out_dir, round(lr_sched.get_last_lr()[0], 6), device)
         torch.save(model.state_dict(), f'{out_dir}/model_{epoch}.pth')
